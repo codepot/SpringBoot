e62e49b2f95eb584600ae585c40181d19e42d0a9
codepot <lekongfuc@yahoo,.com>
2019-08-09 20:07:15 -0700

    Create a SpringBoot Project using Thymeleaf server-side Java template engine

--- spring-boot-demo/src/main/java/demo/HelloController.java
+++ spring-boot-demo/src/main/java/demo/HelloController.java
@@ -1,7 +1,10 @@
 package demo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
@@ -9,6 +12,12 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Welcome! Greetings from Spring Boot!";
+    }
+    
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
     
 
---
+++ spring-boot-demo/src/main/java/demo/hello/GreetingController.java
@@ -0,0 +1,32 @@
+package demo.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/hi")
+    public String index() {
+        return "Hi! Greetings from Spring Boot!";
+    }
+    
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+    
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
+    
+   
+
+}
